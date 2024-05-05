3e690e810175fe7bfcbff3b29c8f8154aa222ad7
fernandapao <fernandapao_@hotmail.com>
2024-05-05 19:44:02 -0300

    cree la tarjeta de los pokemon y la funcion mostrar

--- script.js
+++ script.js
@@ -28,4 +28,42 @@ function showModal(pokemonj) {
         <p>Habilidades: ${pokemon.abilities.map(ability => ability.ability.name).join(', ')}</p>
         <p>Experiencia base: ${pokemon.base_experience}</p>
     `;
+
+    // Función para cerrar el modal
+function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
 }
+
+
+}
+
+// Función para crear tarjeta de los Pokemons
+function createPokemonCard(pokemon) {
+    const card = document.createElement('div');
+    card.classList.add('pokemon-card');
+    card.innerHTML = `
+        <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+        <h3>${pokemon.name}</h3>
+        <button class="detail-btn">Detalle</button>
+    `;
+    const detailBtn = card.querySelector('.detail-btn');
+    detailBtn.addEventListener('click', () => showModal(pokemon));
+    return card;
+}
+
+// Función para mostrar la lista de Pokémon
+function showPokemonList() {
+    fetchPokemon().then(pokemonDetails => {
+        const pokemonContainer = document.getElementById('pokemon-container');
+        pokemonDetails.sort((a, b) => a.id - b.id);
+        pokemonContainer.innerHTML = '';
+        pokemonDetails.forEach(pokemon => {
+            const card = createPokemonCard(pokemon);
+            pokemonContainer.appendChild(card);
+        });
+    });
+}
+
+// Cargar la lista de Pokémon al cargar la página
+window.onload = showPokemonList;
